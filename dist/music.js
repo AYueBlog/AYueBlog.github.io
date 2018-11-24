c8e82f9dfd788ccb6f0d5eead433fdab3661fb24
ayue <ayue9387@163.com>
2018-11-25 01:08:12 +0800

    Site updated: 2018-11-25 01:08:12

---
+++ dist/music.js
@@ -0,0 +1,23 @@
+const ap = new APlayer（{ 
+    container：document.getElementById（'player'），
+    listFolded：false，//列表默认折叠
+     listMaxHeight：90，//列表最大高度
+     lrcType：3，//此为歌词格式，没有歌词可以直接删掉这一行
+     音频：[ 
+        { name：'Way Back Home'，            艺术家：'SHAUN'，            url：'http://music.163.com/song/media/outer/url?id=863046037.mp3'，            封面：'http://p1.music.126.net/MAkLvm2p9LE0mWLEr2NkMA==/109951163378634466.jpg'，            lrc：'http://music.163.com/api/song/media?id=863046037'，主题：'＃ebd0c2'         }，
+            
+
+
+
+
+            
+
+        { name：'Love Yourself'，            artist：'Justin Bieber'，            url：'http://music.163.com/song/media/outer/url?id=36496695.mp3'，            cover：'http://p1.music.126.net/SmaGv_bQTYy5_uOmnAz4Mw==/3285340746015046.jpg?param=130y130'，            lrc：'http://music.163.com/api/song/media?id=36496695'，theme：'＃46718b'         }     ] }）;
+            
+
+
+
+
+            
+
+
